fde74acc2ec4aef2978f2b1c31045b034fcd6917
Alessandro De Pietri Tonelli <adepietri.it@gmail.com>
2024-07-02 16:39:12 +0200

    update0207

---
+++ src/app/components/event-slide/event-slide.component.ts
@@ -0,0 +1,53 @@
+import { Component, OnInit } from '@angular/core';
+import { Event } from '../../models/event'
+import { Router } from '@angular/router';
+import { AuthService } from '../../services/auth.service';
+import { EventService } from '../../services/event.service';
+import { CommonModule } from '@angular/common';
+
+@Component({
+  selector: 'app-event-slide',
+  standalone: true,
+  imports: [CommonModule],
+  templateUrl: './event-slide.component.html',
+  styleUrl: './event-slide.component.css'
+})
+export class EventSlideComponent implements OnInit{
+
+  events: Event[] = [];
+  isAuthenticated: boolean = false;
+
+  constructor(
+    private eventService: EventService,
+    private authService: AuthService,
+    private router: Router
+  ) {}
+
+  ngOnInit(): void {
+    this.loadEvents();
+    this.authService.isAuthenticated$.subscribe(isAuthenticated => {
+      this.isAuthenticated = isAuthenticated;
+    });
+  }
+
+  loadEvents(): void {
+    this.eventService.getEvents().subscribe({
+      next: response => {
+        this.events = response.data;
+      },
+      error: err => {
+        console.error('Error loading events', err);
+      }
+    });
+  }
+
+  navigateToEvent(eventId: number): void {
+    if (this.isAuthenticated) {
+      this.router.navigate(['/events', eventId]);
+    } else {
+      localStorage.setItem('redirectUrl', `/events/${eventId}`);
+      this.router.navigate(['/login']);
+    }
+  }
+
+}
--- src/app/components/hero-section/hero-section.component.ts
+++ src/app/components/hero-section/hero-section.component.ts
@@ -1,12 +1,13 @@
 import { Component } from '@angular/core';
 import { RouterLink } from '@angular/router';
+import { EventSlideComponent } from "../event-slide/event-slide.component";
 
 @Component({
-  selector: 'app-hero-section',
-  standalone: true,
-  imports: [RouterLink],
-  templateUrl: './hero-section.component.html',
-  styleUrl: './hero-section.component.css'
+    selector: 'app-hero-section',
+    standalone: true,
+    templateUrl: './hero-section.component.html',
+    styleUrl: './hero-section.component.css',
+    imports: [RouterLink, EventSlideComponent]
 })
 export class HeroSectionComponent {
   scrollToAbout(): void {
